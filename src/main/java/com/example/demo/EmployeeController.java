a8b52dfb6d5ec13d412468c1187a6ada711aff29
Tian Zhang <975571814@qq.com>
2018-05-03 18:19:44 +0800

    write add employee API

---
+++ src/main/java/com/example/demo/EmployeeController.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "employees")
+public class EmployeeController {
+  @Autowired
+  private List<Employee> employees;
+
+  @RequestMapping(value = "", method = RequestMethod.POST)
+  public ResponseEntity addEmployee(@RequestBody Employee employee) {
+    employee.setId(employees.size());
+    employees.add(employee);
+
+    return new ResponseEntity(HttpStatus.CREATED);
+  }
+}
